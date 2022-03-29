170488dd6ffc50056fc5b9e4080e1df2e70e812e
Prasheel Reddy <spsaiprasheel@gmail.com>
2022-03-29 22:00:09 +0530

    aws lambda

---
+++ src/fetchTodo.js
@@ -0,0 +1,26 @@
+"use strict";
+const AWS = require("aws-sdk")
+
+const fetchTodo = async (event) => {
+    const dynamodb = new AWS.DynamoDB.DocumentClient()
+    const { id } = event.pathParameters
+    let todo
+    try {
+        const result = await dynamodb.get({
+            TableName: "TodoTable",
+            Key: { id }
+        }).promise()
+        todo = result.Item
+    } catch (e) {
+        console.log(e)
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(todo),
+    };
+};
+
+module.exports = {
+    handler: fetchTodo
+}
